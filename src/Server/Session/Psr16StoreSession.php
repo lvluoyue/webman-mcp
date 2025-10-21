b4ea101e7412d6c5a5ddb471302d9f235284ce7d
落月 <1569097443@qq.com>
2025-10-21 23:20:31 +0800

    feat(server): add psr16Session prefix

--- src/Server/Session/Psr16StoreSession.php
+++ src/Server/Session/Psr16StoreSession.php
@@ -12,6 +12,7 @@ class Psr16StoreSession implements SessionStoreInterface
 
     public function __construct(
         private readonly CacheInterface $cache,
+        private readonly string $prefix = 'mcp-',
         private readonly int $ttl = 3600
     )
     {
@@ -20,7 +21,7 @@ class Psr16StoreSession implements SessionStoreInterface
     public function exists(Uuid $id): bool
     {
         try {
-            return $this->cache->has($id->toRfc4122());
+            return $this->cache->has($this->getKey($id));
         } catch (Throwable) {
             return false;
         }
@@ -29,7 +30,7 @@ class Psr16StoreSession implements SessionStoreInterface
     public function read(Uuid $id): string|false
     {
         try {
-            return $this->cache->get($id->toRfc4122(), false);
+            return $this->cache->get($this->getKey($id), false);
         } catch (Throwable) {
             return false;
         }
@@ -38,7 +39,7 @@ class Psr16StoreSession implements SessionStoreInterface
     public function write(Uuid $id, string $data): bool
     {
         try {
-            return $this->cache->set($id->toRfc4122(), $data, $this->ttl);
+            return $this->cache->set($this->getKey($id), $data, $this->ttl);
         } catch (Throwable) {
             return false;
         }
@@ -47,7 +48,7 @@ class Psr16StoreSession implements SessionStoreInterface
     public function destroy(Uuid $id): bool
     {
         try {
-            return $this->cache->delete($id->toRfc4122());
+            return $this->cache->delete($this->getKey($id));
         } catch (Throwable) {
             return false;
         }
@@ -56,5 +57,10 @@ class Psr16StoreSession implements SessionStoreInterface
     public function gc(): array
     {
         return [];
+    }
+
+    private function getKey(Uuid $id): string
+    {
+        return $this->prefix . $id;
     }
 }
